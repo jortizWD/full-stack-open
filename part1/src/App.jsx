94d81df7256b3db396691e37e6ba61310af3fddd
Jortiz <jortiz@aplyca.com>
2024-12-08 10:36:03 -0500

    fix(part1): Part 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,27 +3,29 @@ import Footer from "./Footer.jsx";
 import Content from "./Content.jsx";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content parts={parts}></Content>
-      <Footer parts={parts}></Footer>
+      <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Footer parts={course.parts}></Footer>
     </div>
   );
 };
