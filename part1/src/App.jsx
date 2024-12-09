1b5413b3a290dd2e93dd1126a99e15af96f91808
Jortiz <jortiz@aplyca.com>
2024-12-08 19:53:46 -0500

    fix(part1): Part 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,7 +6,23 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const total = good + neutral + bad;
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {total}</p>
+      <p>Avarge {(good - bad) / total}</p>
+      <p>Positive {(100 * good) / total} %</p>
+    </div>
+  );
 };
 
 export default App;
