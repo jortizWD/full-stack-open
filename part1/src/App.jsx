66c2bc7a255e2bc48972d9a2231caa913a7b783f
Jortiz <jortiz@aplyca.com>
2024-12-08 19:39:56 -0500

    fix(part1): Part 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,33 +1,12 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Content from "./Content.jsx";
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      <Header course={course.name}></Header>
-      <Content parts={course.parts}></Content>
-      <Footer parts={course.parts}></Footer>
-    </div>
-  );
+  return <div>code here</div>;
 };
 
 export default App;
