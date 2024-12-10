f8c740a19eed3fb79e789e3618a2ece0cef93f42
Jortiz <jortiz@aplyca.com>
2024-12-09 21:19:29 -0500

    fix(part1): Part 1.11

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,23 +6,25 @@ const Button = ({ onClick, children }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>{text}</td> <td>{value}</td>
+    </tr>
   );
 };
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   return (
-    <>
-      <StatisticLine text={"Good"} value={good} />
-      <StatisticLine text={"Neutral"} value={neutral} />
-      <StatisticLine text={"Bad"} value={bad} />
-      <StatisticLine text={"All"} value={total} />
-      <StatisticLine text={"Avarge"} value={(good - bad) / total} />
-      <StatisticLine text={"Positive"} value={`${(100 * good) / total} %`} />
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text={"Good"} value={good} />
+        <StatisticLine text={"Neutral"} value={neutral} />
+        <StatisticLine text={"Bad"} value={bad} />
+        <StatisticLine text={"All"} value={total} />
+        <StatisticLine text={"Avarge"} value={(good - bad) / total} />
+        <StatisticLine text={"Positive"} value={`${(100 * good) / total} %`} />
+      </tbody>
+    </table>
   );
 };
 
