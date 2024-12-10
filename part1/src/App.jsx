0e3c30ccdb2c80fb22f8cde63645b64e5d372267
Jortiz <jortiz@aplyca.com>
2024-12-09 21:41:35 -0500

    fix(part1): Part 1.12

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -7,7 +7,8 @@ const Button = ({ onClick, children }) => {
 const StatisticLine = ({ text, value }) => {
   return (
     <tr>
-      <td>{text}</td> <td>{value}</td>
+      <td>{text}</td>
+      <td>{value}</td>
     </tr>
   );
 };
@@ -34,10 +35,25 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [selected, setSelected] = useState(0);
+
+  const getAnecdote = () => {
+    setSelected(Math.round(Math.random() * (anecdotes.length - 1)));
+  };
+
   return (
     <div>
       <h1>Give feedback</h1>
-      <Button onClick={() => setGood(good + 1)}>Good</Button>
       <Button onClick={() => setNeutral(neutral + 1)}>Neutral</Button>
       <Button onClick={() => setBad(bad + 1)}>Bad</Button>
       <h1>Statistics</h1>
@@ -46,6 +62,9 @@ const App = () => {
       ) : (
         <p>No Feeback given</p>
       )}
+      <h1>Anecdotes</h1>
+      <Button onClick={getAnecdote}>Next Anecdote</Button>
+      <p>{anecdotes[selected]}</p>
     </div>
   );
 };
